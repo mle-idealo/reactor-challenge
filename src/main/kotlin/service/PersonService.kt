7d2c66f08be3311d3fb61924a3a2cc2be15b8279
mle-idealo <80523547+mle-idealo@users.noreply.github.com>
2021-09-10 14:05:12 +0200

    Use defaults as intermediate null substitute

--- src/main/kotlin/service/PersonService.kt
+++ src/main/kotlin/service/PersonService.kt
@@ -8,12 +8,18 @@ class PersonService(
     private val ageService: AgeService,
     private val hairService: HairService
 ) {
-    fun create() : Mono<PersonDTO> {
+    fun create(): Mono<PersonDTO> {
+        val default = -1
         val name = nameService.get()
-        val age = ageService.get()
-        val hair = hairService.get()
+        val age = ageService.get().defaultIfEmpty(default)
+        val hair = hairService.get().defaultIfEmpty(default)
 
-        // TODO: Implement this method according to its unit tests: PersonServiceTest
-        return Mono.empty()
+
+        return Mono.zip(name, age, hair)
+            .map {
+                val nullableAge = if (it.t2 == default) null else it.t2
+                val nullableHair = if (it.t3 == default) null else it.t3
+                PersonDTO(it.t1, nullableAge, nullableHair)
+            }
     }
 }
